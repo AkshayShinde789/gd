6031d893ef9d13387122de169b371236066bccde
Akshay <a2shinde1999@gmail.com>
2023-04-20 22:12:04 +0530

    all code gd night

---
+++ Conditional Statement/Program.cs
@@ -0,0 +1,23 @@
+﻿//Conditional Statements
+
+class Conditionals
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a number..... ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if(number == 5000)
+        {
+            Console.WriteLine("You enter : 5000");
+        }
+/*
+        Console.WriteLine($"You Entered :{number}");
+        Console.WriteLine($"You Entered :Ten");
+        Console.WriteLine($"You Entered :three");
+        Console.WriteLine($"You Entered :two");*/
+
+
+        Console.ReadLine();
+    }
+}
--- gd/Program.cs
+++ gd/Program.cs
@@ -17,7 +17,7 @@ Collapse:- ctrl+m+o
         Console.WriteLine("Batch 20 GD");
     }
 }*/
-#endregion 
+#endregion
 
 
 // Type Conversion
@@ -41,35 +41,120 @@ Collapse:- ctrl+m+o
 
 // 2. Explicit :-
 
-class Program
+//class Program
+//{
+//    static void Main()
+//    {
+//        #region Cast
+//        //long v1 = 123456789109273;
+//        //short v2 =(short) v1;
+
+//        decimal v1 = 1235454565745657.567m;
+//        float v2 = (short)v1;
+
+//        //long v1 = 123;
+//        //short v2 =(short) v1;
+//        #endregion
+
+//        // COvert class method
+
+//string number = "100";
+//int a = number; ......error
+//int a = Convert.ToInt32(number);
+
+//        //string s1 = "Sagar";
+//        //int a =Convert.ToInt32 (s1);
+//        //a = (int)s1;
+
+
+//        Console.WriteLine(a);
+
+//        Console.ReadLine();
+//    }
+
+//}
+//Coversion USing parse or tryparse method
+
+class program
 {
     static void Main()
     {
-        #region Cast
-        //long v1 = 123456789109273;
-        //short v2 =(short) v1;
+        //string number = "100";  // string => int
 
-        decimal v1 = 1235454565745657.567m;
-        float v2 = (short)v1;
-
-        //long v1 = 123;
-        //short v2 =(short) v1;
-        #endregion
-
-        // COvert class method
-
-        //string number = "100";
-        ////int a = number; ......error
-        //int a= Convert.ToInt32(number);
-
-        //string s1 = "Sagar";
-        //int a =Convert.ToInt32 (s1);
-        //a = (int)s1;
+        //int i2 = int.Parse(number);
+        //Console.WriteLine(i2);
 
 
-        Console.WriteLine(a);
+        //number = "False";
+        //bool b2=bool.Parse(number);
+        //Console.WriteLine(b2);
+
+        //number = "fols";
+        //bool b3=bool.Parse(number);
+        //Console.WriteLine(b3);
+
+        //TryParse
+
+        //int number = "fols";
+
+        //bool b3;
+        //bool b4=bool.TryParse(number, out b3);
+
+        //string a = "200abc";
+        //int i;
+        //bool b=int.TryParse(a, out i);
+
+
+/*     Datatype         Default Value
+
+        int                 0
+        double              0
+        bool               False
+        String              null
+        Array               null
+        class               null
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        Console.WriteLine($"{b}  {i}");
+
+
+
 
         Console.ReadLine();
-    }
 
+
+
+
+
+
+
+        //int i2 =(int)number;
+
+        //int i3=Convert.ToInt32(number);
+        //Console.WriteLine(i3);
+
+
+
+    }
 }
